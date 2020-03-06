8f0244aee0638b89d7d6839c7316663ed312263a
Cornelius <cornelius.rott@gmail.com>
2020-03-06 14:59:31 -0700

    Show functionality

--- 02-dom/src/scripts/main.js
+++ 02-dom/src/scripts/main.js
@@ -4,7 +4,11 @@ import functions from './functions.js';
 
 // Event listener for SIZE app
 
-mainDiv.addEventListener('click', ((e) => {
-    console.log(e.target);
-}));
+// mainDiv.addEventListener('click', ((e) => {
+//     console.log(e.target);
+// }));
 
+show.addEventListener('click', (() => {
+    // console.log(document.getElementsByTagName("li").textContent)
+    functions.showChildren()
+}));
