af34f1e7e40eb23ea9837466b17bf540601eac21
Cornelius <cornelius.rott@gmail.com>
2020-03-08 20:30:49 -0600

    Added Working with Cards files

---
+++ 02-dom/src/scripts/functions-cards.js
@@ -0,0 +1,44 @@
+const ol = document.getElementById("list");
+
+const functions = {
+           
+        showChildren: () => {
+            const c = document.querySelector("ol").children
+            const text = "";
+            for (let i=0; i < c.length; i++) {
+                text += c[i].textContent + " ";
+            }
+            textOutput.textContent = text
+        }, 
+
+        addClick: () => {
+            if (input.value.length > 0) {
+                functions.createListItem();
+            }
+        }, 
+        
+        addEnter: () => {
+            if (input.value.length > 0 && event.keyCode === 13) {
+                functions.createListItem();
+            }
+        }, 
+
+        createListItem: () => {
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(input.value)); 
+            ol.appendChild(li);
+            input.value = "";
+        },
+        
+        deleteListItem: (delItem) => {
+            ol.removeChild(delItem);
+        }, 
+
+        strikeOutListItem: (strikeItem) => {
+            strikeItem.classList.toggle("strike");
+        }
+
+};
+
+
+export default functions;
