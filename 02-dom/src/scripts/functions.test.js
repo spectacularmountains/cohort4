d06c8aeecdd5a67b7a9e4f1819841cd9700541fa
Cornelius <cornelius.rott@gmail.com>
2020-03-10 10:06:33 -0600

    Updated tests

--- 02-dom/src/scripts/functions.test.js
+++ 02-dom/src/scripts/functions.test.js
@@ -1,5 +1,19 @@
 import functions from './functions'
 
+test('Test the DOM', () => {
+    document.body.innerHTML =
+    '<div class="mainBox" id="mainDiv">' +
+        '<ol class="orderedList" id="list">' +
+            '<li>Item 1</li>' +
+            '<li>Item 2</li>' +
+            '<li>Item 3</li>' +
+        '</ol>'
+    expect(document.querySelector("ol").textContent).toBe("Item 1Item 2Item 3"); 
+    expect(document.querySelector("li").textContent).toBe("Item 1"); 
+    expect(document.getElementsByClassName("mainBox").length).toEqual(1); 
+
+});
+
 test('Find children from ol (ordered list)', () => {
     document.body.innerHTML =
     '<div class="mainBox" id="mainDiv">' +
@@ -8,7 +22,8 @@ test('Find children from ol (ordered list)', () => {
             '<li>Item 2</li>' +
             '<li>Item 3</li>' +
         '</ol>'
-    console.log(list.children.value)
-    // expect(functions.showChildren(document.getElementById("list").children)).toBe("Item1Item2Item3"); 
- 
+    expect(document.querySelector("ol").textContent).toBe("Item 1Item 2Item 3"); 
+    expect(document.querySelector("li").textContent).toBe("Item 1"); 
+    expect(document.getElementsByClassName("mainBox").length).toEqual(1); 
+
 });
