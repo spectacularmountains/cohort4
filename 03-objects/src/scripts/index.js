b1d8ae0e2993343a50673d6b83e3da612ac8bbaf
spectacularmountains <cornelius.rott@gmail.com>
2020-03-14 15:56:29 -0600

    Started deposit button

--- 03-objects/src/scripts/index.js
+++ 03-objects/src/scripts/index.js
@@ -1,2 +1,14 @@
 import Account from './account.js'
 
+const cheqAcc = new Account("Chequings Account", 25);
+
+idBalance.textContent = (cheqAcc.balance()).toFixed(2);
+idAccount.textContent = cheqAcc.accName;
+
+
+// EVENT LISTENER FOR DEPOSIT BUTTON 
+
+buttonDeposit.addEventListener("click", (() => {
+	
+	idBalance.textContent = cheqAcc.deposit(idDeposit.value);
+}));
