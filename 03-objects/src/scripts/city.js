7989eb5f1ed36a30fa197da88cb59ee7c8ffaded
spectacularmountains <cornelius.rott@gmail.com>
2020-04-24 10:45:13 -0600

    Completed show and create functionalities

--- 03-objects/src/scripts/city.js
+++ 03-objects/src/scripts/city.js
@@ -1,16 +1,32 @@
+const url = 'http://localhost:5000/';
+let data; 
+
+
 // CREATE CLASS "CITY"
 
 class City {
-	constructor (name, latitude, longitude, population) {
-		this.name = name;
+	constructor (key, city, population, latitude, longitude) {
+		this.key = key;
+		this.city = city;
+		this.population = population; 
 		this.latitude = latitude; 
 		this.longitude = longitude; 
-		this.population = population; 
 	};
 
-	show () {
-        let string = this.name + this.latitude + this.longitude + this.population; 
-        return string;
+	static async show (city) {
+        const cityList = document.querySelector("#cityList");
+        const row = document.createElement("tr"); 
+        row.innerHTML = `
+            <td>${city.city}</td>
+            <td>${city.population}</td>
+            <td>${city.latitude}</td>
+            <td>${city.longitude}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+            `
+        cityList.appendChild(row); 
+        console.log(1000)
+
+        return;
 	}; 
 
 	movedIn (number) {
@@ -23,7 +39,7 @@ class City {
 		return this.population;
 	};
 
-	howBig (inhabitants) {
+	static howBig (inhabitants) {
         if (inhabitants > 100000) { return "City"; };
         if (inhabitants > 20000) { return "Large town"; };
         if (inhabitants > 1000) { return "Town"; };
@@ -51,12 +67,27 @@ class Controller {
         return "Ushuaia"; // Placeholder for now! 
     };
 
-    getPopulation () {
-        return "7200000000"; // Placeholder for now! 
+    static async getPopulation () {
+        let totalPopulation = 0;
+        let numberOfCities = (await postData(url + 'all')).length; 
+        for (let i=1; i<numberOfCities; i++) {
+        data = await postData(url + 'read', {key: i});
+        totalPopulation += data[0].population;
+        }
+    textOutput.innerHTML = totalPopulation;
     };
 
-    createCity (cityName) {
-        return "This city has been created: " + cityName;  
+    static async createCity () {
+        cityList.innerHTML = "";
+        const city = document.getElementById("city").value; 
+        const population = Number(document.getElementById("population").value); 
+        const latitude = Number(document.getElementById("latitude").value); 
+        const longitude = Number(document.getElementById("longitude").value); 
+        const key = (await postData(url + 'all')).length + 1; 
+        
+        const newCity = new City(key, city, population, latitude, longitude); // Instantiate new City 
+        data = await postData(url + 'add', newCity);
+        City.show(newCity);
     }; 
 
     deleteCity (cityName) {
@@ -65,5 +96,28 @@ class Controller {
 }
 
 
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',       // no-cors, *cors, same-origin
+            cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow',         // manual, *follow, error
+            referrer: 'no-referrer',    // no-referrer, *client
+            body: JSON.stringify(data)  // body data type must match "Content-Type" header
+        });
 
-export {City, Controller}
+        const json = await response.json();    // parses JSON response into native JavaScript objects
+        json.status = response.status;
+        json.statusText = response.statusText;
+        // console.log(json, typeof(json));
+        if (response.status > 200) {return false};
+        return json;
+}
+
+export {City, Controller, postData}
--- 03-objects/src/scripts/index-city.js
+++ 03-objects/src/scripts/index-city.js
@@ -1,23 +1,25 @@
-import {City, Controller} from './city.js'
+import {City, Controller, postData} from './city.js'
 
 
 const url = 'http://localhost:5000/';
 let data; 
+let totalPopulation = 0;
 
 
 
 const cities = [
-    {key:1, city:"Tokyo", latitude: 35.6850, longitude: 139.7514, population: 9273000},
-    {key:2, city:"New York", latitude: 40.6943, longitude: -73.9249, population: 8399000},
-    {key:3, city:"Mexico City", latitude: 19.4424, longitude: -99.1310, population: 8855000},
-    {key:4, city:"Mumbai", latitude: 19.0170, longitude: 72.8570, population: 18410000},
-    {key:5, city:"Sao Paulo", latitude: -23.5587, longitude: -46.6250, population: 12180000},
+    {key:1, city:"Tokyo", population: 9273000, latitude: 35.6850, longitude: 139.7514},
+    {key:2, city:"New York", population: 8399000, latitude: 40.6943, longitude: -73.9249},
+    {key:3, city:"Mexico City", population: 8855000, latitude: 19.4424, longitude: -99.1310},
+    {key:4, city:"Mumbai", population: 18410000, latitude: 19.0170, longitude: 72.8570},
+    {key:5, city:"Sao Paulo", population: 12180000, latitude: -23.5587, longitude: -46.6250},
 ]
 
     // Check that the server is running and clear any data
 const clear = async () => {
     data = await postData(url + 'clear');
     if (data.status > 200) { console.log("ERROR!")}
+    cityList.innerHTML = "";
     textOutput.innerHTML = `<h3>Database cleared.<h3>`;
 };
 
@@ -29,20 +31,30 @@ const populate = async () => {
     textOutput.innerHTML = `<h3>Added ${cities.length} entries to the database.</h3>`;
 };
 
-const show = async () => {
-    let output = ""; 
-    for (let i=0; i<cities.length; i++) {
-        data = await postData(url + 'read', cities[i]);
-        if (data === false) {textOutput.innerHTML = `<h3>No data loaded on server!</h3>`; return} 
-        
-        output +=   `<h3>${cities[i].key}: ${cities[i].city}</h3>
-                    <ul>
-                        <li>Latitude: ${cities[i].latitude}</li>
-                        <li>Longitude: ${cities[i].longitude}</li>
-                        <li>Population: ${cities[i].population}</li>
-                    </ul>`
-    }; 
-    textOutput.innerHTML = output;
+const showAll = async () => {
+    cityList.innerHTML = "";
+    const numberOfCities = (await postData(url + 'all')).length; 
+
+    for (let i=0; i<numberOfCities; i++) {
+        data = await postData(url + 'read', {key: i+1});
+        if (data === false) {
+            textOutput.innerHTML = `<h3>No data loaded on server!</h3>`; 
+            return;
+        } 
+        else {
+            console.log(data[0])
+        City.show(data[0]);
+        };
+    };
+
+    //     output +=   `<h3>${cities[i].key}: ${cities[i].city}</h3>
+    //                 <ul>
+    //                     <li>Latitude: ${cities[i].latitude}</li>
+    //                     <li>Longitude: ${cities[i].longitude}</li>
+    //                     <li>Population: ${cities[i].population}</li>
+    //                 </ul>`
+    // }; 
+    // textOutput.innerHTML = output;
 };
 
 const search = async (e) => {
@@ -82,92 +94,67 @@ const add = () => {
     console.log("Add new city")
 };
 
-/*
-    data = await postData(url + 'all');
-    expect(data.status).toEqual(200);
-    expect(data.length).toBe(0);
-
-    data = await postData(url + 'add', clients[0]);
-    expect(data.status).toEqual(200);
-
-    data = await postData(url + 'all');
-    expect(data.status).toEqual(200);
-    expect(data.length).toBe(1);
-    expect(data[0].name).toBe("Larry");
-
-    // add a second with the same key which should be an error
-    data = await postData(url + 'add', clients[0]);
-    expect(data.status).toEqual(400);
-
-    // add a second which should be ok
-    data = await postData(url + 'add', clients[1]);
-    expect(data.status).toEqual(200);
-
-    data = await postData(url + 'all');
-    expect(data.status).toEqual(200);
-    expect(data.length).toBe(2);
-    expect(data[1].name).toBe("Lorraine");
-
-    data = await postData(url + 'read', {key:1});
-    expect(data.status).toEqual(200);
-    expect(data.length).toBe(1);
-    expect(data[0].name).toBe("Larry");
-
-    data = await postData(url + 'update', {key:1, name:"George"});
-    expect(data.status).toEqual(200);
-
-    data = await postData(url + 'read', {key:1});
-    expect(data.status).toEqual(200);
-    expect(data.length).toBe(1);
-    expect(data[0].name).toBe("George");
-
-    data = await postData(url + 'delete', {key:1});
-    expect(data.status).toEqual(200);
-
-    data = await postData(url + 'read', {key:1});
-    expect(data.status).toEqual(400);
+const howBig = () => {
+    let pop = Number(idInputHowBig.value);
+    let cityType = City.howBig(pop); 
+    textOutput.innerHTML = cityType;
 };
-*/ 
 
-async function postData(url = '', data = {}) {
-    // Default options are marked with *
-        const response = await fetch(url, {
-            method: 'POST',     // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors',       // no-cors, *cors, same-origin
-            cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
-            headers: {
-                'Content-Type': 'application/json'
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            redirect: 'follow',         // manual, *follow, error
-            referrer: 'no-referrer',    // no-referrer, *client
-            body: JSON.stringify(data)  // body data type must match "Content-Type" header
-        });
 
-        const json = await response.json();    // parses JSON response into native JavaScript objects
-        json.status = response.status;
-        json.statusText = response.statusText;
-        // console.log(json, typeof(json));
-        if (response.status > 200) {return false};
-        return json;
-}
+// async function postData(url = '', data = {}) {
+//     // Default options are marked with *
+//         const response = await fetch(url, {
+//             method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+//             mode: 'cors',       // no-cors, *cors, same-origin
+//             cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+//             credentials: 'same-origin', // include, *same-origin, omit
+//             headers: {
+//                 'Content-Type': 'application/json'
+//                 // 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             redirect: 'follow',         // manual, *follow, error
+//             referrer: 'no-referrer',    // no-referrer, *client
+//             body: JSON.stringify(data)  // body data type must match "Content-Type" header
+//         });
+
+//         const json = await response.json();    // parses JSON response into native JavaScript objects
+//         json.status = response.status;
+//         json.statusText = response.statusText;
+//         // console.log(json, typeof(json));
+//         if (response.status > 200) {return false};
+//         return json;
+// }
 
 
 // EVENTLISTENERS 
+
+// document.addEventListener("DOMContentLoaded", showAll);
 
 idButtonPopulate.addEventListener("click", populate);
 
 idButtonClear.addEventListener("click", clear);
 
-idButtonShow.addEventListener("click", show);
+idButtonShow.addEventListener("click", showAll);
 
-idInputCity.addEventListener("keyup", search); 
+city.addEventListener("keyup", search); 
 
 idButtonAdd.addEventListener("click", checkIfCityExists);
 
 idInputAdd.addEventListener("keyup", (e) => {
-    if(e.keyCode === 13) {
-        checkIfCityExists();
-    }
+    if(e.keyCode === 13) {checkIfCityExists()}
 });
+
+idButtonHowBig.addEventListener("click", howBig);
+
+idInputHowBig.addEventListener("keyup", (e) => {
+    if(e.keyCode === 13) {howBig()}
+});
+
+idButtonTotalPopulation.addEventListener("click", (() => {
+Controller.getPopulation(); 
+}));
+
+cityForm.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    Controller.createCity();
+})
